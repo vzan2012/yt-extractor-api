612bc54a03bba54b5a517fcc9c2e4891227c52cd
Deepak Guptha <vzan2012@gmail.com>
2023-07-16 16:13:27 +0530

    fix: cors issue by adding frontend url

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: true,
+    cors: {
+      origin: process.env.FRONTEND_URL,
+    },
   });
 
   // Config Swagger Module
