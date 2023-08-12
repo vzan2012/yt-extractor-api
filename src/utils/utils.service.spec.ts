f95d640e1ed3a2ce71a0c2f49024fe6795d18605
Deepak Guptha <vzan2012@gmail.com>
2023-08-12 19:15:17 +0530

    feat: update test cases for utils

--- src/utils/utils.service.spec.ts
+++ src/utils/utils.service.spec.ts
@@ -3,7 +3,10 @@ import { UtilsService } from './utils.service';
 import { UtilsModule } from './utils.module';
 
 describe('UtilsService', () => {
-  let service: UtilsService;
+  let utilsService: UtilsService;
+
+  const englishWord = 'fileNameString';
+  const fileId = 'lqHq6_SM-0c';
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,10 +14,18 @@ describe('UtilsService', () => {
       providers: [UtilsService],
     }).compile();
 
-    service = module.get<UtilsService>(UtilsService);
+    utilsService = module.get<UtilsService>(UtilsService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('isEnglishLanguage() - to Be Defined', () => {
+    expect(utilsService).toBeDefined();
+  });
+
+  it('isEnglishLanguage() - returns boolean true', () => {
+    expect(utilsService.isEnglishLanguage(englishWord)).toBeTruthy();
+  });
+
+  it('getUpdatedFileName() - to Be Defined', () => {
+    expect(utilsService.getUpdatedFileName(fileId, englishWord)).toBeDefined();
   });
 });
