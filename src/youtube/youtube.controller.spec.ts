7e68701dcc8d422a438e92200554b66d3f4bbf34
Deepak Guptha <vzan2012@gmail.com>
2023-08-14 23:26:53 +0530

    feat: added tests for youtube controller

--- src/youtube/youtube.controller.spec.ts
+++ src/youtube/youtube.controller.spec.ts
@@ -4,20 +4,54 @@ import { UtilsModule } from '../utils/utils.module';
 import { UtilsService } from '../utils/utils.service';
 import { YoutubeService } from './youtube.service';
 
+import * as ytdl from 'ytdl-core';
+import { YtdlMock } from '../../test/__mocks__/@nestjs/ytdl.mock';
+import { fileId, mockInfo } from '../../test/__mocks__/inputs';
+import { HttpException, HttpStatus } from '@nestjs/common';
+
 describe('YoutubeController', () => {
-  let controller: YoutubeController;
+  let youtubeController: YoutubeController;
+  let youtubeService: YoutubeService;
+  let ytdlMock: typeof ytdl;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UtilsService, YoutubeService],
+      providers: [YtdlMock, UtilsService, YoutubeService],
       controllers: [YoutubeController],
       imports: [UtilsModule],
     }).compile();
 
-    controller = module.get<YoutubeController>(YoutubeController);
+    ytdlMock = module.get<typeof ytdl>(ytdl);
+    youtubeController = module.get<YoutubeController>(YoutubeController);
+    youtubeService = module.get<YoutubeService>(YoutubeService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('youtube controller', () => {
+    it('youtube controller should be defined', () => {
+      expect(youtubeController).toBeDefined();
+    });
+  });
+
+  describe('getFileInfoById', () => {
+    it('should return Promise', () => {
+      ytdlMock.getInfo = jest.fn().mockResolvedValue(mockInfo);
+
+      expect(youtubeController.getFileInfo(fileId)).toBeInstanceOf(Promise);
+    });
+    it('should be throw an exception', async () => {
+      const invalidFileId = 'invalidId';
+      const errorMessage = `Video id (${invalidFileId}) does not match expected format (/^[a-zA-Z0-9-_]{11}$/)`;
+
+      jest
+        .spyOn(ytdlMock, 'getInfo')
+        .mockRejectedValue(new Error(errorMessage));
+
+      try {
+        await youtubeController.getFileInfo(invalidFileId);
+        fail('Expecting an exception to throw error');
+      } catch (error) {
+        expect(error.message).toEqual(errorMessage);
+      }
+    });
   });
 });
